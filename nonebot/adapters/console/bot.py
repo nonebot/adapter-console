674eb3be9e9969e5da7fcee822cbe86ca4d10ccd
CAESIUS_TIM <60285058+CAESIUS-TIM@users.noreply.github.com>
2023-04-27 11:17:22 +0800

    🐛 change to keyword arguments
    `user_id` and `message` are keyword arguments.

--- nonebot/adapters/console/bot.py
+++ nonebot/adapters/console/bot.py
@@ -32,7 +32,7 @@ def _check_nickname(bot: "Bot", event: MessageEvent) -> None:
 class Bot(BaseBot):
     if TYPE_CHECKING:
 
-        async def send_msg(self, user_id: str, message: Message, **kwargs: Any) -> None:
+        async def send_msg(self, *, user_id: str, message: Message, **kwargs: Any) -> None:
             ...
 
         async def bell(self) -> None:
